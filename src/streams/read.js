cd397fc8b27025ee3789391bf4f03e1cd97634ac
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 17:18:58 +0300

    feat: add read and write solutions in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(filePath);
+  readStream.on('data', (chunk) => process.stdout.write(chunk.toString()));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToWrite.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const write = async () => {
-    // Write your code here 
+  let dataToWrite = '';
+
+  const writeStream = fs.createWriteStream(filePath);
+
+  process.stdin.on('data', (data) => {
+    dataToWrite += data;
+  });
+
+  process.on('SIGINT', () => {
+    writeStream.write(dataToWrite);
+    writeStream.close();
+    process.exit()
+  });
 };
 
 await write();
