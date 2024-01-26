a830eafef2a60342beb1e2c1c28960cafb9075d4
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 16:57:58 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,37 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToCalculateHashFor.txt';
+
+const ALGORITHM = 'sha256';
+const ENCODING_OF_RESULT = 'hex';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  const readStream = fs.createReadStream(filePath);
+  const hash = crypto.createHash(ALGORITHM);
+
+  readStream.on('data', data => {
+    hash.update(data);
+  });
+
+  readStream.on('end', () => {
+    const resHash = hash.digest(ENCODING_OF_RESULT);
+    console.log(`${filePath} hash is ${resHash}`);
+  });
+
+  readStream.on('error', error => {
+    throw error;
+  });
+}
+
+
+
 
 await calculateHash();
