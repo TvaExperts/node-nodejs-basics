0897c37e756535bb08986570c7700a70b78e8e30
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 20:51:05 +0300

    feat: add zip solutions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToCompress.txt';
+const ARCHIVE_FILE_NAME = 'archive.gz';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileNamePath = join(__dirname, FOLDER_NAME, FILE_NAME);
+const archiveFileNamePath = join(__dirname, FOLDER_NAME, ARCHIVE_FILE_NAME);
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(archiveFileNamePath);
+  const writeStream = fs.createWriteStream(fileNamePath);
+  const gzip = zlib.createGunzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log(`File ${fileNamePath} has been decompressed to ${archiveFileNamePath}`);
+  });
 };
 
 await decompress();
