2e39b1a0ecb77e1ffbd13b94d295d461e3864522
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 12:41:04 +0300

    feat: add create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
+import { open, close, write } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fresh.txt';
+
+const filePath = path.join(__dirname, FOLDER_NAME ,FILE_NAME);
+
+const TEXT_VALUE = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  open(filePath, 'wx', (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error('FS operation failed');
+      }
+      throw err;
+    }
+    try {
+      write(fd, TEXT_VALUE,(err) => {
+        if (err) throw err;
+      })
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await create();
