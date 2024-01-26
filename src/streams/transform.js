8c43acf47d6d5468bd48a21cac92f988f3178e63
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 20:38:08 +0300

    feat: add transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+
+class ReverseTransform extends Transform {
+  constructor() {
+    super();
+  }
+
+  _transform(chunk, encoding, callback) {
+    const reversedChunk = chunk.toString().trim().split('').reverse().join('');
+    this.push(`${reversedChunk}\n`);
+    callback();
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
-};
+  const reverseTransform = new ReverseTransform();
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
+}
 
 await transform();
