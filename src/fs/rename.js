d63b242fb435ae84cd4c6c543f30ae77dda60b1e
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-26 13:26:12 +0300

    feat: add rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,38 @@
+import { constants } from 'fs';
+import fsPromises from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'wrongFilename.txt';
+const NEW_FILE_NAME = 'properFilename.md';
+
+const fileNamePath = path.join(__dirname, FOLDER_NAME, FILE_NAME);
+const newFileNamePath = path.join(__dirname, FOLDER_NAME, NEW_FILE_NAME);
+
+
 const rename = async () => {
-    // Write your code here 
+
+  try {
+    await fsPromises.access(fileNamePath, constants.F_OK);
+
+    try {
+      await fsPromises.access(newFileNamePath, constants.F_OK);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+    await fsPromises.rename(fileNamePath, newFileNamePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+    throw err;
+  }
 };
 
 await rename();
