059323451af1b5c8325e85414efe9da967f64ddc
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-28 01:35:50 +0300

    feat: add wt solution

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,14 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from 'worker_threads';
+
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+// if (Math.random() > 0.5) throw new Error('A-A-A-A-A');
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // const startTime = new Date().getTime();
+  const value = nthFibonacci(workerData);
+  // const duration = new Date().getTime() - startTime;
+  parentPort.postMessage({ /* num: workerData, */ value /*, duration */ });
 };
 
 sendResult();
