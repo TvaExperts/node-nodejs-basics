369370fb9ab3aa4bbeebcc6be37e80db11c1479d
Trachenko Viktor <trachenko.v.a@gmail.com>
2024-01-29 10:38:13 +0300

    fix: fix warnings of import json file

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -2,6 +2,8 @@ import path from 'path';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 import { fileURLToPath } from 'url';
+import { createRequire } from 'node:module';
+
 import './files/c.js';
 
 const PATH_FILE_A = './files/a.json';
@@ -12,15 +14,7 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const random = Math.random();
-
-let unknownObject;
-
-if (random > 0.5) {
-  unknownObject = await import(PATH_FILE_A, { with: { type: 'json' } });
-} else {
-  unknownObject = await import(PATH_FILE_B, { with: { type: 'json' } });
-}
+const require = createRequire(import.meta.url);
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -29,11 +23,13 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
+const random = Math.random();
+const unknownObject = random >= 0.5 ? require(PATH_FILE_A) : require(PATH_FILE_B);
+console.log(unknownObject);
+
 const myServer = createServerHttp((_, res) => {
   res.end('Request accepted');
 });
-
-console.log(unknownObject);
 
 myServer.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
